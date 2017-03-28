be593b3e17cea1431709754d5c781e740fe847c5
Xavier <touchaville@gmail.com>
2017-03-28 14:55:47 +0200

    modified scripts.js prostowanie choinki

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 function rysujChoinke(n) {
     for (var i = 1; i <= n; i++) {
-    		var star = '*';
-			
-		for (var j = 1 ; j < i*2-1; j++) {
-			star += '*';
-		}
-		console.log(star)
+        var star = '';
+		var space = '';	
+			for (var j=i; j<n; j++) {
+				space += ' ';
+			}
+			for (var j = 0; j < i * 2 - 1; j++) {
+				star += '*';
+			}
+			console.log(space, star)
 	}
 }
-
-rysujChoinke(5)
+rysujChoinke(16)
